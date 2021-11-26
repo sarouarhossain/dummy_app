83c98d4d5877397fecaf29c254e16acb05c3d8c6
Hossain, Sarouar | Sarouar | ADPDE <sarouar.hossain@rakuten.com>
2021-11-26 18:33:17 +0900

    a

--- frontend/my-app/src/http-common.js
+++ frontend/my-app/src/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:5050"
+const API_BASE_URL = process.env.API_BASE_URL || "http://178.128.16.172:5050"
 export default axios.create({
     withCredentials: true,
     baseURL: API_BASE_URL,
